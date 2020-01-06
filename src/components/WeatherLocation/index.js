882d617cc768afc43c674a2c573df222b2c3bce1
Daniel Hoyas <hmartindaniel@gmail.com>
2020-01-06 22:01:26 +0100

    call to weather api

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -10,6 +10,12 @@ const data = {
     wind: '10 m/s'
 }
 
+const location = "Madrid, es";
+const api_key = "dd2b496f5430517a515f84f8458cd9ec";
+const url_base_weather = "http://api.openweathermap.org/data/2.5/weather";
+
+const api_weather = `${url_base_weather}?q=${location}&appid=${api_key}`;
+
 class WeatherLocation extends Component  {
 
     constructor() {
@@ -20,7 +26,54 @@ class WeatherLocation extends Component  {
         };
     }
 
+    getWeatherState = weather_data => {
+        return SUN;
+    }
+
+    getData = weather_data => {
+        const { humidity, temp} = weather_data.main;
+        const { speed } = weather_data.wind;
+        const weatherState = this.getWeatherState;
+
+        const data = {
+            humidity,
+            temperature: temp,
+            weatherState,
+            wind: `${speed} m/s`,
+        }
+
+        return data;
+    }
+
     handleUpdateClick = () => {
+
+        fetch(api_weather)
+        .then( resolve => {
+            //console.log(resolve);
+            return resolve.json();
+        })
+        .then( data => {
+            const newWeather = this.getData(data);
+            console.log(newWeather);
+            this.setState({
+                data: newWeather
+            });
+
+        })
+        .catch( err => {
+            console.log(err);
+        });
+
+        //let weather_promise = fetch(api_weather);
+        // weather_promise.then( (data) => {
+        //     console.log('exito');
+        //     console.log(data);
+        // } )
+        // .catch( (err) => {
+        //     console.log('error');
+        //     console.log(err);
+        // } );
+
         console.log('actualizado');
         this.setState({
             city: 'BCN',
